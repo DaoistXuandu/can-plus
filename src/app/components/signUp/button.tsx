8baeb09ef44f76ee0d2a65996969af9150cb6c5a
raihan <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>
2024-08-01 20:25:47 +0700

    minor: fix login signin button

--- src/app/components/signUp/button.tsx
+++ src/app/components/signUp/button.tsx
@@ -2,8 +2,8 @@ import { submit } from "@/app/lib/content/signUp";
 
 export default function Button() {
     return (
-        <button className=" w-full mt-16 flex flex-col space-y-2">
-            <h1 style={{ backgroundColor: "#F1C40F" }} className="w-full text-center font-bold text-2xl p-3 rounded-xl">{submit.button}</h1>
+        <div className=" w-full mt-16 flex flex-col space-y-2">
+            <button style={{ backgroundColor: "#F1C40F" }} className="hover:scale-90 w-full text-center font-bold text-2xl p-3 rounded-xl">{submit.button}</button>
             <div className="text-sm flex flex-row space-x-1">
                 <p>
                     {submit.login.comment.normal}
@@ -12,6 +12,6 @@ export default function Button() {
                     {submit.login.comment.bold}
                 </a>
             </div>
-        </button >
+        </div >
     )
 }
